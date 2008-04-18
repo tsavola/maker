62a98c8a7329ff93dcda3eaf800e00005b89da92
Timo Savola <timo.savola@iki.fi>
2008-04-18 23:29:11 +0300

    separate file for plugins

--- build/library.mk
+++ build/library.mk
@@ -1,11 +1,8 @@
 include build/common.mk
 include build/target.mk
 
-ifeq ($(LIBRARY),)
 LIBRARY		:= lib$(NAME).so.$(VERSION)
 LIBRARY_LINK	:= lib$(NAME).so
-endif
-
 ARCHIVE		:= lib$(NAME).a
 
 O_LIBRARY	:= $(O)/lib/$(LIBRARY)
@@ -13,20 +10,18 @@ O_LIBRARY_LINK	:= $(O)/lib/$(LIBRARY_LINK)
 O_ARCHIVE	:= $(O)/lib/$(ARCHIVE)
 
 build: build-shared build-static
-build-shared: $(O)/lib/$(LIBRARY)
-build-static: $(O)/lib/$(ARCHIVE)
+build-shared: $(O_LIBRARY)
+build-static: $(O_ARCHIVE)
 install: install-shared install-static
 
-$(O)/lib/$(LIBRARY): $(PIC_OBJECTS) $(DEPENDS)
+$(O_LIBRARY): $(PIC_OBJECTS) $(DEPENDS)
 	$(call echo,Link,$@)
 	$(QUIET) mkdir -p $(dir $@)
 	$(QUIET) $(LINKCOMMAND) -fPIC -shared -Wl,-soname,$(LIBRARY) \
 		-o $@ $(PIC_OBJECTS) $(LIBS)
-ifneq ($(LIBRARY_LINK),)
 	$(QUIET) ln -sf $(LIBRARY) $(O_LIBRARY_LINK)
-endif
 
-$(O)/lib/$(ARCHIVE): $(OBJECTS)
+$(O_ARCHIVE): $(OBJECTS)
 	$(call echo,Archive,$@)
 	$(QUIET) mkdir -p $(dir $@)
 	$(QUIET) $(AR) crs $@ $(OBJECTS)
@@ -35,9 +30,7 @@ install-shared:
 ifneq ($(wildcard $(O_LIBRARY)),)
 	mkdir -p $(DEST_LIBDIR)
 	install $(O_LIBRARY) $(DEST_LIBDIR)/
-ifneq ($(LIBRARY_LINK),)
 	ln -sf $(LIBRARY) $(DEST_LIBDIR)/$(LIBRARY_LINK)
-endif
 endif
 
 install-static:
---
+++ build/plugin.mk
@@ -0,0 +1,18 @@
+include build/common.mk
+include build/target.mk
+
+PLUGIN		:= $(NAME).so
+O_PLUGIN	:= $(O)/plugin/$(PLUGIN)
+
+build: $(O_PLUGIN)
+
+$(O_PLUGIN): $(PIC_OBJECTS) $(DEPENDS)
+	$(call echo,Link,$@)
+	$(QUIET) mkdir -p $(dir $@)
+	$(QUIET) $(LINKCOMMAND) -fPIC -shared -o $@ $(PIC_OBJECTS) $(LIBS)
+
+install:
+	mkdir -p $(DEST_PLUGINDIR)
+	install $(O_PLUGIN) $(DEST_PLUGINDIR)/
+
+.PHONY: build install
