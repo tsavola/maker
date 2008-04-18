e7d3b8814e64d35553f512a8ddff10b1125672de
Timo Savola <timo.savola@iki.fi>
2008-04-19 01:03:26 +0300

    build: use the 'install' program and clean up

--- build/binary.mk
+++ build/binary.mk
@@ -1,16 +1,17 @@
 include build/common.mk
 include build/target.mk
 
-build: $(O)/bin/$(NAME)
+O_NAME		:= $(O)/bin/$(NAME)
 
-$(O)/bin/$(NAME): $(OBJECTS) $(DEPENDS)
+build: $(O_NAME)
+
+$(O_NAME): $(OBJECTS) $(DEPENDS)
 	$(call echo,Link,$@)
 	$(QUIET) mkdir -p $(dir $@)
 	$(QUIET) $(LINKCOMMAND) -o $@ $(OBJECTS) $(LIBS)
 
 install:
 	mkdir -p $(DEST_BINDIR)
-	cp $(O)/bin/$(NAME) $(DEST_BINDIR)/
-	chmod 755 $(DEST_BINDIR)/$(NAME)
+	install $(O_NAME) $(DEST_BINDIR)/
 
 .PHONY: build install
--- build/test.mk
+++ build/test.mk
@@ -1,7 +1,7 @@
 include build/binary.mk
 
 check: build
-	$(call echo,Check,$(O)/bin/$(NAME))
-	$(QUIET) LD_LIBRARY_PATH=$(O)/lib $(O)/bin/$(NAME)
+	$(call echo,Check,$(O_NAME))
+	$(QUIET) LD_LIBRARY_PATH=$(O)/lib $(O_NAME)
 
 .PHONY: check
