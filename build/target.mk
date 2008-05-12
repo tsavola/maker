fa5383444209ca46aa926865de8577c50f057b7c
Timo Savola <timo.savola@iki.fi>
2008-05-13 00:40:54 +0300

    build: modular c/c++ compilation rules

--- build/target.mk
+++ build/target.mk
@@ -1,66 +1,15 @@
 -include build/pkgconfig.mk
 
-OBJECTS		:= $(SOURCES:%.c=$(O)/obj/%.o)
-PIC_OBJECTS	:= $(SOURCES:%.c=$(O)/obj/%.os)
-
-CXXSUFFIX	:= $(call lastword,$(subst ., ,$(filter $(CXXPATTERNS),$(SOURCES))))
-
-ifeq ($(CXXSUFFIX),)
-LINKCOMMAND	:= $(CCLINKER)  $(CFLAGS) $(CCFLAGS)  $(LDFLAGS)
-else
-LINKCOMMAND	:= $(CXXLINKER) $(CFLAGS) $(CXXFLAGS) $(LDFLAGS)
-
-OBJECTS		:= $(OBJECTS:%.$(CXXSUFFIX)=$(O)/obj/%.o)
-PIC_OBJECTS	:= $(PIC_OBJECTS:%.$(CXXSUFFIX)=$(O)/obj/%.os)
-endif
-
 CONFIGFLAGS	:= $(if $(CONFIG),-include $(CONFIG),)
 GENERICFLAGS	:= $(CPPFLAGS) $(CONFIGFLAGS) $(CFLAGS) -c -MD
-CCCOMMAND	:= $(CC)  $(GENERICFLAGS) $(CCFLAGS)
-CXXCOMMAND	:= $(CXX) $(GENERICFLAGS) $(CXXFLAGS)
 PICFLAGS	:= -DPIC -fPIC
 
-$(O)/obj/%.o: %.c
-	$(call echo,Compile,$@)
-	$(QUIET) mkdir -p $(dir $@)
-	$(QUIET) $(CCCOMMAND) \
-		-MF $(O)/obj/$(patsubst %,%.d,$*.c) \
-		-MT $(O)/obj/$*.o -MT $(O)/obj/$*.os \
-		-o $@ $*.c
+-include build/c++.mk
+-include build/c.mk
 
-$(O)/obj/%.os: %.c
-	$(call echo,Compile,$@)
-	$(QUIET) mkdir -p $(dir $@)
-	$(QUIET) $(CCCOMMAND) \
-		$(PICFLAGS) \
-		-MF $(O)/obj/$(patsubst %,%.d,$*.c) \
-		-MT $(O)/obj/$*.o -MT $(O)/obj/$*.os \
-		-o $@ \
-		$*.c
-
-ifneq ($(CXXSUFFIX),)
-
-$(O)/obj/%.o: %.$(CXXSUFFIX)
-	$(call echo,Compile,$@)
-	$(QUIET) mkdir -p $(dir $@)
-	$(QUIET) $(CXXCOMMAND) \
-		-MF $(O)/obj/$(patsubst %,%.d,$*.$(CXXSUFFIX)) \
-		-MT $(O)/obj/$*.o -MT $(O)/obj/$*.os \
-		-o $@ \
-		$*.$(CXXSUFFIX)
-
-$(O)/obj/%.os: %.$(CXXSUFFIX)
-	$(call echo,Compile,$@)
-	$(QUIET) mkdir -p $(dir $@)
-	$(QUIET) $(CXXCOMMAND) \
-		$(PICFLAGS) \
-		-MF $(O)/obj/$(patsubst %,%.d,$*.$(CXXSUFFIX)) \
-		-MT $(O)/obj/$*.o -MT $(O)/obj/$*.os \
-		-o $@ \
-		$*.$(CXXSUFFIX)
-
-endif
-
-$(OBJECTS) $(PIC_OBJECTS): $(CONFIG)
+OBJECTS		:= $(patsubst %,$(O)/obj/%.o,$(basename $(SOURCES)))
+PIC_OBJECTS	:= $(patsubst %,$(O)/obj/%.os,$(basename $(SOURCES)))
 
 -include $(SOURCES:%=$(O)/obj/%.d)
+
+$(OBJECTS) $(PIC_OBJECTS): $(CONFIG)
