dc34af32e0ad53fd1ce838afc70315fbc37156f5
Timo Savola <timo.savola@iki.fi>
2008-05-28 20:49:34 +0300

    build: fix c++ compilation

--- build/c++.mk
+++ build/c++.mk
@@ -5,20 +5,20 @@ ifneq ($(CXXSUFFIX),)
 CXXCOMMAND	:= $(CXX) $(GENERICFLAGS) $(CXXFLAGS)
 LINKCOMMAND	:= $(CXXLINKER) $(CFLAGS) $(CXXFLAGS) $(LDFLAGS)
 
-$(O)/obj/%.o: %.$(CXXSUFFIX)
+$(O)/obj/%.o: %$(CXXSUFFIX)
 	$(call echo,Compile,$@)
 	$(QUIET) mkdir -p $(dir $@)
 	$(QUIET) $(CXXCOMMAND) \
-		-MF $(O)/obj/$(patsubst %,%.d,$*.$(CXXSUFFIX)) \
+		-MF $(O)/obj/$(patsubst %,%.d,$*$(CXXSUFFIX)) \
 		-MT $(O)/obj/$*.o -MT $(O)/obj/$*.os \
-		-o $@ $*.$(CXXSUFFIX)
+		-o $@ $*$(CXXSUFFIX)
 
-$(O)/obj/%.os: %.$(CXXSUFFIX)
+$(O)/obj/%.os: %$(CXXSUFFIX)
 	$(call echo,Compile,$@)
 	$(QUIET) mkdir -p $(dir $@)
 	$(QUIET) $(CXXCOMMAND) $(PICFLAGS) \
-		-MF $(O)/obj/$(patsubst %,%.d,$*.$(CXXSUFFIX)) \
+		-MF $(O)/obj/$(patsubst %,%.d,$*$(CXXSUFFIX)) \
 		-MT $(O)/obj/$*.o -MT $(O)/obj/$*.os \
-		-o $@ $*.$(CXXSUFFIX)
+		-o $@ $*$(CXXSUFFIX)
 
 endif
