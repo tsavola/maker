f34c28acab726f3138683a5dc70b42dc7c376d13
Timo Savola <timo.savola@iki.fi>
2008-05-29 12:24:43 +0300

    build: fix c++ linking

--- build/target.mk
+++ build/target.mk
@@ -4,8 +4,8 @@ CONFIGFLAGS	:= $(if $(CONFIG),-include $(CONFIG),)
 GENERICFLAGS	:= $(CPPFLAGS) $(CONFIGFLAGS) $(CFLAGS) -c -MD
 PICFLAGS	:= -DPIC -fPIC
 
--include build/c++.mk
 -include build/c.mk
+-include build/c++.mk
 
 OBJECTS		:= $(patsubst %,$(O)/obj/%.o,$(basename $(SOURCES)))
 PIC_OBJECTS	:= $(patsubst %,$(O)/obj/%.os,$(basename $(SOURCES)))
