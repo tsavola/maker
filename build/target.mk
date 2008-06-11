06926f50e678fc9e9721114abbe7f8bb0aa1cd33
Timo Savola <timo.savola@iki.fi>
2008-06-11 08:21:21 +0300

    build: check that NAME and SOURCES are specified for targets

--- build/target.mk
+++ build/target.mk
@@ -1,3 +1,6 @@
+$(if $(NAME),,$(error NAME not specified for target))
+$(if $(SOURCES),,$(error SOURCES not specified for target))
+
 -include build/pkgconfig.mk
 
 CONFIGFLAGS	:= $(if $(CONFIG),-include $(CONFIG),)
